12b776464916a809d0fe8ebdb8643111416d6b24
punjabi.gabru <dharmindersingh.gne@gmail.com>
2024-11-21 21:22:08 +0000

    done

--- calls/calls.js
+++ calls/calls.js
@@ -3,11 +3,34 @@ function SetUsername(username) {
 }
 
 function createUsername(username, email, password) {
+	super(username);
 	this.email = email;
 	this.password = password;
 	SetUsername.call(this, username); // we cant access username from above function so we need to
 	// hold the reference with .call method and this would be first parameter.
+
+	// but now we can use super() method instead of .call
 }
 
 const chai = new createUsername("chai", "chai@gmail.com", 1234);
 console.log(chai);
+
+// example of super...
+
+class hello {
+	constructor(username) {
+		this.username = username;
+	}
+
+	logMe() {
+		console.log(`username is ${this.username}`);
+	}
+}
+
+class hello2 extends hello {
+	constructor(username, email, password) {
+		super(username);
+		this.email = email;
+		this.password = password;
+	}
+}
--- classes/classes.js
+++ classes/classes.js
@@ -19,3 +19,23 @@ const hello = new User("sunny", "sunny@gmail.com", 12345);
 //console.log(chai);
 console.log(hello.encryptPass());
 console.log(hello.changeName());
+
+// behind the scene.....
+
+function User1(username, email, password) {
+	this.username = username;
+	this.email = email;
+	this.password = password;
+}
+
+Object.prototype.changeName = function () {
+	return `${this.username.toUpperCase()}`;
+};
+
+Object.prototype.encryptPass = function () {
+	return `${this.password}abcde`;
+};
+
+const tea = new User1("Rink", "rinku@gmail.com", 2345);
+console.log(tea.changeName());
+console.log(tea.encryptPass());
